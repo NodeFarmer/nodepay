445b25d16da488cb74f8be8e042a4f7e6fc28552
NodeFarmer <cryptonodefarmer@gmail.com>
2024-12-03 00:32:44 +0100

    Update nodepay.py

--- nodepay.py
+++ nodepay.py
@@ -37,11 +37,11 @@ all_proxies = filter_non_empty_lines(read_lines_file(os.path.join(script_dir, 'p
 proxy_type = read_single_line_file(os.path.join(script_dir, 'proxy-config.txt')) or 'http'
 
 # Constants
-HTTPS_URL = "http://18.142.29.174/api/network/ping"
+HTTPS_URL = "https://nw2.nodepay.org/api/network/ping"
 RETRY_INTERVAL = 60  # Retry interval for failed proxies in seconds
 EXTENSION_VERSION = "2.2.7"
 GITHUB_REPO = "NodeFarmer/nodepay"
-CURRENT_VERSION = "1.3.6"
+CURRENT_VERSION = "1.3.7"
 NODEPY_FILENAME = "nodepay.py"
 
 # Function to download the latest version of the script
